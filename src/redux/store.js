e057992cd679123b42abc993000a87d9d485afcd
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-07 02:20:43 +0300

    Configure store.js file and add bookSlice

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import bookSlice from "./books/bookSlice";
+
+
+const store = configureStore({
+    reducer:{
+        books:bookSlice,
+    }
+})
+
+export default store
