e42f01ce61598ea7aded456f3818567af8f2245c
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-07 02:30:41 +0300

    Create categorieSlice

---
+++ src/redux/categories/categorieSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    books: [],
+    status: "Under construction",
+}
+
+const categorieSlice = createSlice({
+    name:"categories",
+    initialState,
+    reducers: {
+        checkStatus: (state)=> ({
+            ...state,
+            status: "Under constraction"
+        })
+    }
+})
+
+export const { checkStatus } = categorieSlice.actions
+export default categorieSlice.reducer
