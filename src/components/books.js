d71051d8de605aaf042a9c2210ae01eaa5d25af1
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-08 06:02:06 +0300

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import '../styles/books.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeBook } from '../redux/books/bookSlice';
+import { getBooks, delBook } from '../redux/books/bookSlice';
 import Input from './input';
 
 const Books = () => {
   const { books } = useSelector((store) => store.books);
+  const precentage = Math.floor(Math.random() * 100);
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
   return (
     <div className="main-container">
       <div className="books-container">
         {books.map((book) => (
-          <ul key={book.id} className="book-list">
+          <ul key={book.item_id} book={book} className="book-list">
             <li><strong>{book.title}</strong></li>
             <li>{book.author}</li>
             <div className="btns">
@@ -19,7 +23,7 @@ const Books = () => {
               <button
                 type="button"
                 className="rmBtn"
-                onClick={() => dispatch(removeBook(book.id))}
+                onClick={() => dispatch(delBook(book.item_id))}
               >
                 Remove
               </button>
